eead40c282fdb8341126fd939bb31aa8b6544c26
dlstjd5945 <dlstjd5945@gmail.com>
2020-09-08 15:27:00 +0000

    new files

---
+++ courses.py
@@ -0,0 +1,14 @@
+a_string = "like this"
+a_number = 3
+a_float = 3.12
+a_boolean = False
+a_none = None
+
+print(type(a_number))
+print(type(a_float))
+print(type(a_boolean))
+print(type(a_string))
+print(type(a_none))
+
+super_long_variable = True
+print(super_long_variable)
